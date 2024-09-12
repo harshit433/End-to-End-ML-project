de9ffe25a164a64bdb9438eae74fcc2175c62443
Harshit Aggarwal <67017828+harshit433@users.noreply.github.com>
2024-09-12 20:33:53 +0530

    changes

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import pickle
+import dill
 import sys
 import os
 from src.exception import CustomException
@@ -10,7 +10,7 @@ def save_object(file_path, obj):
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path, 'wb') as file:
-            pickle.dump(obj, file)
+            dill.dump(obj, file)
     except Exception as e:
         raise CustomException(e,sys)
     
