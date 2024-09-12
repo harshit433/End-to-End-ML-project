b2a7b8cdee70ea125d2a5ecd052e3eb8aa5b02bc
Harshit Aggarwal <67017828+harshit433@users.noreply.github.com>
2024-09-12 13:26:07 +0530

    exception and logging

---
+++ src/exception.py
@@ -0,0 +1,14 @@
+import sys
+
+def error_message_detail(error, error_detail : sys):
+    _,_,error_tb = error_detail.exc_info()
+    error_message = 'error occured in python script named [{0}] line number [{1}] and error message is [{2}]'.format(error_tb.tb_frame.f_code.co_filename, error_tb.tb_lineno, error)
+    return error_message
+           
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail :sys):
+        super.__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message   
